faf44b7e5a13ff7cf88c78c6e764c9d5657566b5
dinithimendis <dinithimendis42@gmail.com>
2023-11-01 09:01:58 +0530

    content update

--- Frontent/controllers/LoginFormController.js
+++ Frontent/controllers/LoginFormController.js
@@ -2,4 +2,10 @@ let baseURL = "http://localhost:8080/Backend_war/";
 
 $("#login").on('click', function () {
 
+    $.ajax({
+        url: baseURL + "login_form/?userName=" + $("#name").val() + "&password=" + $("#password").val(),
+        method: "GET",
+        dataType: "json",
+    });
+
 });
