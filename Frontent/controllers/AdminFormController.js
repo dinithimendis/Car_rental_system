e5acdc376674a85c4e3f357dc4e465ea848f9e75
dinithimendis <dinithimendis42@gmail.com>
2023-11-01 10:54:58 +0530

    admincontroller update

--- Frontent/controllers/AdminFormController.js
+++ Frontent/controllers/AdminFormController.js
@@ -15,6 +15,20 @@ function saveAdmin() {
         url: baseURL + "save_admin", method: "post", data: formData, dataType: "json", success: function (res) {
             getAllAdmins();
 
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                background:'#27ae60',
+                showConfirmButton: false,
+                color: "#fff",
+                timer: 3000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.addEventListener('mouseenter', Swal.stopTimer)
+                    toast.addEventListener('mouseleave', Swal.resumeTimer)
+                }
+            })
+
         }
 
     });
