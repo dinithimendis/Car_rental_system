71970a9a103527f87114c65239b3007cdc589505
dinithimendis <dinithimendis42@gmail.com>
2023-11-01 22:08:48 +0530

    update customer

--- Frontent/controllers/CustomerFormController.js
+++ Frontent/controllers/CustomerFormController.js
@@ -59,3 +59,82 @@ function saveCustomer() {
     });
     clearTextFields();
 }
+
+$("#updateCustomer").on('click', function () {
+
+    let id = $('#id').val();
+    let firstName = $('#firstName').val();
+    let lastName = $('#lastName').val();
+    let address = $('#address').val();
+    let email = $('#email').val();
+    let contactNo = $('#contactNo').val();
+    let user_id = $('#userId').val();
+    let password = $('#password').val();
+    let nic = $('#nic').val();
+    let drivingLicenceNo = $('#drivingLicenseNo').val();
+    let role = $('#role').val();
+    let userName = $('#userName').val();
+
+    var customerObj = {
+        id: id,
+        name: {firstName: firstName, lastName: lastName},
+        address: address,
+        email: email,
+        contactNo: contactNo,
+        user: {userName: userName, userId: user_id, password: password, role: role},
+        nic: nic,
+        drivingLicenseNo: drivingLicenceNo,
+    }
+
+    $.ajax({
+        url: baseURL + "/customer/update_customer",
+        method: "put",
+        contentType: "application/json",
+        data: JSON.stringify(customerObj),
+        dataType: "json",
+        success: function (res) {
+            clearTextFields();
+            getAllCustomers();
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                background:'#27ae60',
+                showConfirmButton: false,
+                color: "#fff",
+                timer: 3000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.addEventListener('mouseenter', Swal.stopTimer)
+                    toast.addEventListener('mouseleave', Swal.resumeTimer)
+                }
+            })
+
+            Toast.fire({
+                icon: 'success',
+                title: 'updated successfully !'
+            });
+        },
+        error: function (error) {
+            // alert(JSON.parse(error.responseText).message);
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                background:'#e70c0c',
+                showConfirmButton: false,
+                color: "#fff",
+                timer: 3000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.addEventListener('mouseenter', Swal.stopTimer)
+                    toast.addEventListener('mouseleave', Swal.resumeTimer)
+                }
+            })
+
+            Toast.fire({
+                icon: 'error',
+                title: 'cannot be update !'
+            });
+
+        }
+    });
+});
