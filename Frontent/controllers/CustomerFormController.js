657f6ded4b60f5c607ab798f8fd1a6dbd8269d8b
dinithimendis <dinithimendis42@gmail.com>
2023-11-01 21:47:54 +0530

    save customer

--- Frontent/controllers/CustomerFormController.js
+++ Frontent/controllers/CustomerFormController.js
@@ -6,3 +6,56 @@ $("#saveCustomer").on('click', function () {
     saveCustomer();
 });
 
+function saveCustomer() {
+    let formData = $("#CustomerFormController").serialize();
+    $.ajax({
+        url: baseURL + "/customer/save_customer",
+        method: "post",
+        data: formData,
+        dataType: "json",
+        success: function (res) {
+            getAllCustomers();
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                background:'#27ae60',
+                showConfirmButton: false,
+                color: "#fff",
+                timer: 3000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.addEventListener('mouseenter', Swal.stopTimer)
+                    toast.addEventListener('mouseleave', Swal.resumeTimer)
+                }
+            })
+
+            Toast.fire({
+                icon: 'success',
+                title: 'Saved successfully !'
+            });
+            window.location.reload();
+        },
+        error: function (error) {
+            var errorMessage = JSON.parse(error.responseText);
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                background:'#e70c0c',
+                showConfirmButton: false,
+                color: "#fff",
+                timer: 3000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.addEventListener('mouseenter', Swal.stopTimer)
+                    toast.addEventListener('mouseleave', Swal.resumeTimer)
+                }
+            })
+
+            Toast.fire({
+                icon: 'error',
+                title: 'cannot be saved !'
+            });
+        }
+    });
+    clearTextFields();
+}
