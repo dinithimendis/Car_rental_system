2db0e7e024983f1c3e76153ac436e7950aa109ca
dinithimendis <dinithimendis42@gmail.com>
2023-11-01 09:02:58 +0530

    content update

--- Frontent/controllers/LoginFormController.js
+++ Frontent/controllers/LoginFormController.js
@@ -6,6 +6,15 @@ $("#login").on('click', function () {
         url: baseURL + "login_form/?userName=" + $("#name").val() + "&password=" + $("#password").val(),
         method: "GET",
         dataType: "json",
+
+        success: function (resp) {
+            console.log($("#name").val());
+            console.log($("#password").val())
+        },
+
+        error: function (error) {
+            alert(JSON.parse(error.responseText).message);
+        }
     });
 
 });
