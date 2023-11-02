df2c10e43e3953e0d19452adeb7aa53a7836181e
dinithimendis <dinithimendis42@gmail.com>
2023-11-02 21:14:06 +0530

    save payment

--- Frontent/controllers/PaymentManageController.js
+++ Frontent/controllers/PaymentManageController.js
@@ -1,1 +1,37 @@
 let baseURL = "http://localhost:8080/Backend_war";
+
+$("#save").on('click', function () {
+
+    let paymentID = $("#paymentId").val();
+    let paymentDate = $("#paymentDate").val();
+    let invoiceNo = $("#invoiceNo").val();
+    let amount = $("#amount").val();
+    let payment = $("#payment").val();
+    let bookingID = $("#bookingId").val();
+
+
+    let obj = {
+        paymentId: paymentID,
+        paymentDate: paymentDate,
+        invoiceNo: invoiceNo,
+        amount: amount,
+        paymentType: payment,
+        bookingDTO: {bookingID: bookingID},
+    }
+
+    $.ajax({
+        url: baseURL + "/payment/save_payment",
+        method: "post",
+        contentType: "application/json",
+        data: JSON.stringify(obj),
+        dataType: "json",
+        success: function (res) {
+            alert(res.message);
+            getAllPaymentDetails();
+        },
+        error: function (error) {
+            var errorMessage = JSON.parse(error.responseText);
+            alert(errorMessage.message);
+        }
+    });
+});
