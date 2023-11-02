0ea70b80bb73fae01234df7896a1ae90e7a19f3e
dinithimendis <dinithimendis42@gmail.com>
2023-11-02 19:59:54 +0530

    monthly revenue

--- Frontent/controllers/IncomeFormController.js
+++ Frontent/controllers/IncomeFormController.js
@@ -21,3 +21,44 @@ function getAllByDailyRevenues() {
         }
     });
 }
+
+function getAllByMonthlyRevenues() {
+    $.ajax({
+        url: baseURL + "income/by_month", dataType: "json", success: function (res) {
+            for (let c of res.data) {
+                if (c[0] === 1) {
+                    month = "January";
+                } else if (c[0] === 2) {
+                    month = "February";
+                } else if (c[0] === 3) {
+                    month = "March";
+                } else if (c[0] === 4) {
+                    month = "April";
+                } else if (c[0] === 5) {
+                    month = "May";
+                } else if (c[0] === 6) {
+                    month = "June";
+                } else if (c[0] === 7) {
+                    month = "July";
+                } else if (c[0] === 8) {
+                    month = "August";
+                } else if (c[0] === 9) {
+                    month = "September";
+                } else if (c[0] === 10) {
+                    month = "October";
+                } else if (c[0] === 11) {
+                    month = "November";
+                } else if (c[0] === 12) {
+                    month = "December";
+                }
+
+                let row = "<tr>" + "<td>" + month + "</td>" + "<td>" + "Rs "+ (c[1])+".00"  + "</td>" + "</tr>";
+                $("#incomeReportsMonthlyTable").append(row);
+
+            }
+        }, error: function (error) {
+            let message = JSON.parse(error.responseText).message;
+            alert(message);
+        }
+    });
+}
