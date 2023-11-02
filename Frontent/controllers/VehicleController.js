8231a052b18be806e8f822b2dd1b06195b7dbd48
dinithimendis <dinithimendis42@gmail.com>
2023-11-02 22:25:40 +0530

    save vehicle

--- Frontent/controllers/VehicleController.js
+++ Frontent/controllers/VehicleController.js
@@ -7,3 +7,59 @@ let genaratedVehicleValue;
 $("#saveVehicle").on('click', function () {
     saveVehicle();
 });
+
+function saveVehicle() {
+    let formData = $("#vehicleFormController").serialize();
+    $.ajax({
+        url: baseURL + "vehicle/save_vehicle",
+        method: "post",
+        data: formData,
+        dataType: "json",
+        success: function (res) {
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                background:'#27ae60',
+                showConfirmButton: false,
+                color: "#fff",
+                timer: 3000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.addEventListener('mouseenter', Swal.stopTimer)
+                    toast.addEventListener('mouseleave', Swal.resumeTimer)
+                }
+            })
+
+            Toast.fire({
+                icon: 'success',
+                title: 'Saved successfully !'
+            });
+            getAllVehicle();
+            clearTextFields();
+            bindRowClickEventsForVehicle();
+        },
+        error: function (error) {
+            var errorMessage = JSON.parse(error.responseText);
+            // alert(errorMessage.message);
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                background:'#e70c0c',
+                showConfirmButton: false,
+                color: "#fff",
+                timer: 3000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.addEventListener('mouseenter', Swal.stopTimer)
+                    toast.addEventListener('mouseleave', Swal.resumeTimer)
+                }
+            })
+
+            Toast.fire({
+                icon: 'error',
+                title: 'Saved Unsuccessful !'
+            });
+        }
+    });
+}
+
