121e9324d81edaa750181ec8e53d853423fd8d5e
dinithimendis <dinithimendis42@gmail.com>
2023-11-02 19:44:47 +0530

    daily renue

--- Frontent/controllers/IncomeFormController.js
+++ Frontent/controllers/IncomeFormController.js
@@ -7,3 +7,17 @@ gettingRevenueByYear();
 let val1;
 let val2;
 
+function getAllByDailyRevenues() {
+    $.ajax({
+        url: baseURL + "income/by_daily", dataType: "json", success: function (res) {
+            for (let c of res.data) {
+                let row = "<tr>" + "<td>" + (c[1]) + "</td>" + "<td>" + "Rs "+(c[0])+".00" + "</td>" + "</tr>";
+                $("#incomeReportsDailyTable").append(row);
+            }
+
+        }, error: function (error) {
+            let message = JSON.parse(error.responseText).message;
+            alert(message);
+        }
+    });
+}
