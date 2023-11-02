bb63e1e4868a48eecfe2a82f5757c759915ac032
dinithimendis <dinithimendis42@gmail.com>
2023-11-02 18:22:38 +0530

    driver shedule complete

--- Frontent/controllers/DriverScheduleController.js
+++ Frontent/controllers/DriverScheduleController.js
@@ -4,3 +4,49 @@ getAllDrivers();
 
 let bookingId;
 let driverId;
+
+function getAllDrivers() {
+    $("#driverTableBody").empty();
+    $.ajax({
+        url: baseURL + "get_driver_schedule", success: function (res) {
+            for (let c of res.data) {
+                bookingId= c[0];
+                driverId= c[1];
+            }
+        }, error: function (error) {
+            let message = JSON.parse(error.responseText).message;
+            alert(message);
+        }
+    });
+    $.ajax({
+        url: baseURL + "get_all", success: function (res) {
+            for (let c of res.data) {
+
+                let id = c.id;
+                let firstname = c.name.firstName;
+                let lastname = c.name.lastName;
+                let drivingLicenseNo = c.drivingLicenseNo;
+                let contactNo = c.contactNo;
+                let nic = c.nic;
+
+                if (id === driverId){
+
+                    let row = "<tr>" +
+                        "<td>" + id + "</td>"
+                        + "<td>" + bookingId + "</td>"
+                        + "<td>" + firstname + "</td>"
+                        + "<td>" + lastname + "</td>"
+                        + "<td>" + drivingLicenseNo + "</td>"
+                        + "<td>" + contactNo + "</td>"
+                        + "<td>" + nic + "</td>"
+                        + "</tr>";
+
+                    $("#driverTableBody").append(row);
+                }
+            }
+        }, error: function (error) {
+            let message = JSON.parse(error.responseText).message;
+            alert(message);
+        }
+    });
+}
